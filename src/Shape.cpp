6ee92f96ff0297bf8d2f6291f7b4c42879397612
Prashant <prashantt492@gmail.com>
2026-01-16 13:39:08 +0530

    first commit

---
+++ src/Shape.cpp
@@ -0,0 +1,48 @@
+class GraphicsObject {
+    private:
+        int height;
+        int width;
+        int x; // top left corner
+        int y; // top left corner
+    public:
+
+
+};
+
+class Line : GraphicsObject {
+
+};
+
+class Rectangle : GraphicsObject {
+
+};
+
+class Hexagon : GraphicsObject {
+
+};
+
+class Circle : GraphicsObject {
+
+};
+
+class RoundedRectangle : GraphicsObject {
+
+};
+
+class Freehand : GraphicsObject {
+
+};
+
+class Text : GraphicsObject {
+
+};
+
+class BoundaryColor : GraphicsObject {
+
+};
+
+class FillColor : GraphicsObject {
+
+};
+
+
